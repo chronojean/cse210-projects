a2b6de615614d4de90a88c6f08509c4234bfaf8a
chronojean <megachronoje@gmail.com>
2024-03-29 00:36:13 -0400

    Created empty classes

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,22 @@
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+    public void Run()
+    {
+
+    }
+    public void GetRandomPrompt()
+    {
+
+    }
+    public List<string> GetListFromUser()
+    {
+        return new List<string>();
+    }
+}
